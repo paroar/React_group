26e6341b11c34313c80934437a0fd388777ac8cd
paroar <prodriguezarmida@gmail.com>
2019-10-28 14:16:52 +0100

    Correcion errores profesor, tipos y asincrono

--- src/components/pages/Details/Details.tsx
+++ src/components/pages/Details/Details.tsx
@@ -1,44 +1,58 @@
 import React, { useState, useEffect } from "react";
+import { RouteComponentProps } from "react-router";
 
-const Details = (match: any) => {
+type Movie = {
+  poster_path: string;
+  backdrop_path: string;
+  
+}
+
+type DetailsProps = RouteComponentProps<{ slug: string }>
+
+const Details: React.FC<DetailsProps> = ({ match: {params: {slug}}}) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+
   useEffect(() => {
+    const fetchMovie = async () => {
+      const data = await fetch(
+        `http://api.themoviedb.org/3/movie/${slug}?api_key=d893f1827f15c0a1128e80650af1466f&append_to_response=videos`
+      );  
+      const item = await data.json();  
+      setMovie(item);
+    };
+  
     fetchMovie();
-  }, []);
+  }, [slug]);
 
-  const [movie, setMovie] = useState([]);
-
-  const fetchMovie = async () => {
-    const data = await fetch(
-      `http://api.themoviedb.org/3/movie/${match.match.params.slug}?api_key=d893f1827f15c0a1128e80650af1466f&append_to_response=videos`
-    );
-
-    const item = await data.json();
-    console.log("<<<<<",item);
-    setMovie(item);
-  };
-
+  
   const imgStyle2 = {
     width: "auto",
     height: "513px"
   };
 
+  if (!movie) {
+    return null;
+  }
+  
   return (
     <div data-testid="details-page">
       <img
-        //@ts-ignore
         src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}/>
-
       <img
         style={imgStyle2}
-        //@ts-ignore
         src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}/>
 
-      {console.log(">>>",movie)}
+      {
+        /*
+              //@ts-ignore*/
+        
+        }
 
-      {/*
-              //@ts-ignore*/}
+      {
+              
       <h4>Title: {movie.title}</h4>
-      {/*
+      {
               //@ts-ignore*/}
       <h4>Rating: {movie.vote_average}</h4>
       {/*
