dea0e461e3b8b3c6c0af47059bb22dc3d530aa93
paroar <prodriguezarmida@gmail.com>
2019-10-29 13:13:32 +0100

    [Card] scss

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 
 const Card: React.FC = movie => {
-  const card = {
-    margin: "auto",
-    position: "relative"
-  };
-
   return (
-    <>
-              
-      <div 
-      //@ts-ignore
-      style={card}>
-        <img
-          //@ts-ignore
-          src={`https://image.tmdb.org/t/p/w185` + movie.movie.poster_path}
-          className="Catalogue--grid--img"
-          alt="img"
-        />
-      </div>
-    </>
+    <div className="card">
+      <img
+        //@ts-ignore
+        src={`https://image.tmdb.org/t/p/w185` + movie.movie.poster_path}
+        className="Catalogue--grid--img"
+        alt="img"
+      />
+    </div>
   );
 };
 
