6e002a6fcfe0edc6bec8412067c1b4934b19af03
paroar <prodriguezarmida@gmail.com>
2019-11-26 18:42:17 +0100

    buscador finalizado

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import { urls } from "../urls";
 import SearchDisplay from "./SearchDisplay";
 
-
 export type SearchMovie = {
-  poster_path: string,
-  title: string,
-}
+  poster_path: string;
+  title: string;
+};
 
 class Search extends React.Component {
-
   state = {
     movies: [] as SearchMovie[],
     searchVersion: 0
-  }
+  };
 
   searchMovies = (title: string) => {
-    
-    const {searchVersion} = this.state;
+    const { searchVersion } = this.state;
 
     const newSearchVersion = searchVersion + 1;
 
@@ -27,32 +24,36 @@ class Search extends React.Component {
       urls.apikey +
       "&language=en-US&page=1&query=" +
       title;
-      this.setState({searchVersion: newSearchVersion}, ()=> {
+    this.setState({ searchVersion: newSearchVersion }, () => {
       fetch(url)
-      .then(response =>response.json())
-      .then(movies=>  {
-        if (this.state.searchVersion === newSearchVersion) {
-          this.setState({movies: movies.results})
-        }
-      })})
-  }
+        .then(response => response.json())
+        .then(movies => {
+          if (this.state.searchVersion === newSearchVersion) {
+            this.setState({ movies: movies.results });
+          }
+        });
+    });
+  };
 
-  handleOnChange(e:  React.ChangeEvent<HTMLInputElement>){
-    if(e.target.value !== ""){
+  handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value !== "") {
       this.searchMovies(e.target.value);
-    //console.log(e.target.value);
     }
   }
-  render(){
-    console.log(this.state.movies)
-    return(
+  render() {
+    return (
       <>
-      <div>
-        <input type="text" placeholder="Search movies" onChange={(e) => this.handleOnChange(e)}/>
-      </div>
-      <SearchDisplay arr={this.state.movies}/>
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="input"
+            onChange={e => this.handleOnChange(e)}
+          />
+        </div>
+        <SearchDisplay arr={this.state.movies} />
       </>
-    )
+    );
   }
 }
 
