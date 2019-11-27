8d47030a0146a89321de15eee2a5a16f5cc85ed2
Alberto <ruizgilalberto@gmail.com>
2019-11-27 23:19:30 +0100

    mejorado el home y cambiada la tipografía, se elimina news

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Hero from "../components/Hero";
-import SearchContainer from "../containers/SearchContainer";
+import { TiHome } from "react-icons/ti";
+import { IoMdSearch, IoMdHelp } from "react-icons/io";
+import { GiFilmSpool } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div data-testid="home-page">
       <Hero hero="homeHero">
-        <SearchContainer/>
+        <h1 className="maintitle">Welcome to 4Birds</h1>
+        <div className="table-icons">
+          <div className="table-icons-icon">
+            <Link to="/"><TiHome size="90"/></Link>
+            <h6>HOME</h6>
+          </div>
+          <div className="table-icons-icon">
+            <Link to="/search"><IoMdSearch size="90"/></Link>
+            <h6>SEARCH</h6>
+          </div>
+          <div className="table-icons-icon">
+            <Link to="/help"><IoMdHelp size="90"/></Link>
+            <h6>HELP</h6>
+          </div>
+          <div className="table-icons-icon">
+            <Link to="/catalogue"><GiFilmSpool size="90"/></Link>
+            <h6>CATALOGUE</h6>
+          </div>
+        </div>
       </Hero>
     </div>
   );
