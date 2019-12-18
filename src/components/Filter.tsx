91c0fc4140c99686f7aca7f17d7d366d1056f671
paroar <prodriguezarmida@gmail.com>
2019-12-18 12:39:48 +0100

    id

---
+++ src/components/Filter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchContainer from "../containers/SearchContainer";
+
+export default function Filter() {
+  return (
+    <div data-testid="news-page">
+      <SearchContainer />
+    </div>
+  );
+}
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,59 @@
 import React from "react";
 import SearchContainer from "../containers/SearchContainer";
+import styled from "styled-components";
+import { ids } from "../utils/ids";
+
+const Genre = styled.div`
+  position: absolute;
+  bottom: 5%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 33%;
+  padding: 0.4rem;
+  background-color: grey;
+  margin: 0.2rem auto;
+  border-radius: 2px;
+  cursor: pointer;
+  transition: all 0.3s;
+  z-index: 2;
+  text-align: center;
+
+  &:hover {
+    background-color: #f76c5e;
+    transform: translate(-50%, -2px);
+  }
+`;
 
 const SearchPage = () => {
+  console.log(ids);
   return (
-    <div data-testid="news-page">
-        <SearchContainer/>
+    <div data-testid="search-page">
+      <SearchContainer />
+      <div className="search-container">
+        <div className="search-container--bar">
+          <input
+            className="search-container--bar__input"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+        <div className="search-container--filter">
+          <select className="search-container--filter--select" name="" id="">
+            <option value="">Valor</option>
+          </select>
+
+          <select className="search-container--filter--select" name="" id="">
+            <option value="">Valor</option>
+          </select>
+
+          <select className="search-container--filter--select" name="" id="">
+            <option value="">Valor</option>
+          </select>
+        </div>
+        <div className="search-container--button">
+          <Genre>Search</Genre>
+        </div>
+      </div>
     </div>
   );
 };
