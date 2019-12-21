a71f4f2be7a0c8c30f4ad8d1765ce0c9fe62b9aa
paroar <prodriguezarmida@gmail.com>
2019-12-21 01:18:34 +0100

    QuickSearch fondo

--- src/components/Navbar/QuickSearch.tsx
+++ src/components/Navbar/QuickSearch.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Poster from "../Poster";
 import { Link } from "react-router-dom";
 import { SearchMovie } from "../../containers/QuickSearchContainer";
@@ -9,13 +9,27 @@ type SearchProps = {
 };
 
 const QuickSearch: React.FC<SearchProps> = props => {
-  if (props.arr) {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.keyCode === 27) {
+        props.handleOnClick();
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+
+  if (props.arr && props.arr.length > 0) {
     return (
-      <div className="search-grid">
+      <div className="search-grid" onClick={props.handleOnClick}>
         {props.arr.map(movie => (
-          <div className="search-grid--movie" onClick={props.handleOnClick}>
+          <div className="search-grid--movie">
             <Link key={movie.id} to={`/catalogue/${movie.id}`}>
-              <Poster imgPath={movie.poster_path}></Poster>
+              {movie.poster_path ? (
+                <Poster imgPath={movie.poster_path} />
+              ) : (
+                <span className="quicksearch-placeholder">No image</span>
+              )}
               <h3>{movie.title}</h3>
               {movie.release_date ? (
                 <h3>{movie.release_date.substring(0, 4)}</h3>
