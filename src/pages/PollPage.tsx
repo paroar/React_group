3c181f04618c6b1ebfdb0cefcd8ff3fab6f0b4f9
paroar <prodriguezarmida@gmail.com>
2020-01-15 23:47:57 +0100

    poll.json

--- src/pages/PollPage.tsx
+++ src/pages/PollPage.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Droppable from "../components/DnD/Droppable";
 import Draggable from "../components/DnD/Draggable";
 import styled from "styled-components";
+import poll from "../assets/poll.json";
 
 const Choose = styled.div`
   background-color: yellowgreen;
   display: flex;
-  justify-content:center;
+  justify-content: center;
   & > * {
     margin: 1rem;
   }
@@ -15,39 +16,19 @@ const Choose = styled.div`
 const PollPage: React.FC = () => {
   return (
     <>
-      <Droppable id="1"></Droppable>
-
-      <Choose>
-        <Draggable id="2">
-          <img src="https://image.tmdb.org/t/p/w500/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg" />
-        </Draggable>
-        <Draggable id="3">
-          <img src="https://image.tmdb.org/t/p/w500/db32LaOibwEliAmSL2jjDF6oDdj.jpg" />
-        </Draggable>
-        <Draggable id="4">
-          <img src="https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" />
-        </Draggable>
-        <Draggable id="5">
-          <img src="https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg" />
-        </Draggable>
-      </Choose>
-
-      <Droppable id="6"></Droppable>
-
-      <Choose>
-        <Draggable id="7">
-          <img src="https://image.tmdb.org/t/p/w500/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg" />
-        </Draggable>
-        <Draggable id="8">
-          <img src="https://image.tmdb.org/t/p/w500/db32LaOibwEliAmSL2jjDF6oDdj.jpg" />
-        </Draggable>
-        <Draggable id="9">
-          <img src="https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" />
-        </Draggable>
-        <Draggable id="10">
-          <img src="https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg" />
-        </Draggable>
-      </Choose>
+      {poll.map(topic => (
+        <>
+          <Droppable id=""></Droppable>
+          <h2>{topic.topic}</h2>
+          <Choose>
+            {topic.nominations.map(nomination => (
+              <Draggable id={nomination.id}>
+                <img src={nomination.path} />
+              </Draggable>
+            ))}
+          </Choose>
+        </>
+      ))}
     </>
   );
 };
