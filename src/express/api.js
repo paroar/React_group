7e30b586b512c90d87f539fba63d389959dc1fff
paroar <prodriguezarmida@gmail.com>
2020-02-10 18:37:25 +0100

    api get nominations

--- src/express/api.js
+++ src/express/api.js
@@ -1,18 +1,29 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var poll = require('./oscarsPoll.json');
+var poll = require("./oscarsPoll.json");
 
-app.get('/getOscarsPoll', function (req, res) {
+app.get("/getOscarsPoll", function(req, res) {
   res.json(poll);
 });
 
-app.get('/getOscarsPoll/topic', function (req, res) {
-  let queryResults = poll.results.filter((t) =>  t.topic.includes(req.query.topic));
-
+app.get("/getOscarsPoll/topic", function(req, res) {
+  let queryResults = poll.results.filter(t =>
+    t.topic.toLowerCase().includes(req.query.topic.toLowerCase())
+  );
   res.json(queryResults);
-  //res.json(poll);
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.get("/getOscarsPoll/nominations", function(req, res) {
+  let queryResults = poll.results.filter(ns =>
+    ns.nominations.filter(n =>
+      n.name
+        .toLocaleLowerCase()
+        .includes(req.query.nominations.toLocaleLowerCase())
+    ).length > 0
+  );
+  res.json(queryResults);
+});
+
+app.listen(3001, function() {
+  console.log("Example app listening on port 3001!");
 });
