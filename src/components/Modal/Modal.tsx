1075c515a6e5b59f9ecd7b851092c9b3b8cd4e50
Ana S <asersic9@gmail.com>
2020-02-12 10:51:54 +0100

    modal - componente mejorado

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Backdrop from "./Backdrop";
+import { FaTimes } from "react-icons/fa";
 
 type ModalProps = {
     show: boolean;
@@ -18,15 +19,16 @@ const Modal = (props: ModalProps) => {
             />
             <div className="modal-wrapper"
                 style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    transform: props.show ? '' : 'translateY(-100vh)',
                     opacity: props.show ? '1' : '0',
                     width: props.modalWidth,
                     left: props.modalLeft
                 }}
             >
-                <div className="aux-wrapper">
-                    {props.children}
+                <div className="close" onClick={props.modalClosed}>
+                    <FaTimes size="2rem" />
                 </div>
+                {props.children}
             </div>
         </div>
     )
